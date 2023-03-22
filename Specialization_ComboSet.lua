7f0d7969d2458d646f6ccb0e23940a36a7d89fe3
Rukstone <18535846+Rukstone@users.noreply.github.com>
2023-03-22 09:32:44 -0300

    Save Spells for Current Specialization
    [Experimental] this only works if you have system messages turn on in chat.
    not the ideal but can work.

--- Specialization_ComboSet.lua
+++ Specialization_ComboSet.lua
@@ -1,6 +1,6 @@
 --------------------------------------------------------------------------------
 ---experimental --- testing use of diferent combo box for fast set up for diferent specs
-HealBot_Config.Selected_CombotSet = "Specialization I"
+
 local Specializations = {
     "Specialization I",
     "Specialization II",
@@ -52,24 +52,36 @@ function Save_ComboBox_Set()
     end
 end
 local function OnPlayerChangeTalentGroup(value,event,msg)
-    if msg then
-        local indexstart, indexend
-        for key, value in pairs(Specializations) do
-            indexstart, indexend = string.find(msg, value .. ".")
-            if indexstart and indexend then
-                if indexstart > 0 and indexend > 0 then
-                    local spe = string.sub(msg, indexstart, string.len(msg) -1)
-                    if spe == value then
-                        Select_Combo_Set(value)
-                        break
+    if event == "CHAT_MSG_SYSTEM" then
+        if msg then
+            local indexstart, indexend
+            for key, value in pairs(Specializations) do
+                indexstart, indexend = string.find(msg, value .. ".")
+                if indexstart and indexend then
+                    if indexstart > 0 and indexend > 0 then
+                        local spe = string.sub(msg, indexstart, string.len(msg) -1)
+                        if spe == value then
+                            Select_Combo_Set(value)
+                            break
+                        end
                     end
                 end
             end
         end
+    elseif event == "PLAYER_ENTERING_WORLD" then
+
+        --Get Player Specialization
     end
+ 
 end
+
 local frame = CreateFrame('Frame');
-frame:RegisterEvent("CHAT_MSG_SYSTEM");  
+frame:RegisterEvent("CHAT_MSG_SYSTEM");
+if HealBot_Config.Selected_CombotSet == nil then
+    HealBot_Config.Selected_CombotSet = "Default-Specialization"
+    frame:RegisterEvent("PLAYER_ENTERING_WORLD");
+
+end
 frame:SetScript("OnEvent", OnPlayerChangeTalentGroup);
 
 ------------------------------------------------------------------------------------------------
